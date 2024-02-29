a3e4711d3feec40ad22ac438a1f66cf19f99f55e
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-03-01 00:25:58 +0530

    Youtube Clone

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{display: "flex", justifyContent:"space-between"}}>
+        <div style={{background:"green" }}>a</div>
+        <div style={{background:"red" }}>b</div>
+        <div style={{background:"orange" }}>c</div>
+      </div>
+    </>
+  )
+}
+
+export default App
